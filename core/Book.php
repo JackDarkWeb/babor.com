4ce8d053b4a7f70bb3ca65efe80f1de95a54ba6c
Jack Dark <jacknouatin1@gmail.com>
2019-11-16 15:30:30 +0100

    updated core

--- core/Book.php
+++ core/Book.php
@@ -9,8 +9,6 @@ class Book
     protected  $date = [];
     protected $msg = "";
 
-    private static $age,
-        $birthDay = '';
 
 
     /**
@@ -218,35 +216,4 @@ class Book
     }
 
 
-    /**
-     * @param object $user
-     * @return int
-     */
-    function getAge($user){
-
-        $date = [$user->day, $user->month, $user->year];
-        $toDay = explode('/', date('d/m/Y'));
-
-        if($date[1] < $toDay[1] || ($date[1] == $toDay[1] && $date[0] <= $toDay[0] )){
-            self::$age = $toDay[2] - $date[2];
-        }else{
-            self::$age = $toDay[2] - $date[2] - 1;
-        }
-        return self::$age;
-    }
-
-    /**
-     * @param object $user
-     * @return string
-     */
-    function getBirthDay($user){
-
-        $date = [$user->day, $user->month, $user->year];
-        $toDay = explode('/', date('d/m/Y'));
-
-        if($date[1] == $toDay[1] && $date[0] == $toDay[0]){
-            self::$birthDay = 'Happy Birth Day';
-        }
-        return self::$birthDay;
-    }
 }
